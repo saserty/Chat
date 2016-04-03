47510664a0f98c1a889a0b4787148fa07d710c1c
Iaroslav Paseka <saserty@ukr.net>
2016-04-03 17:22:01 +0300

    i'm already tired

---
+++ 5th_hw/oop_classes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            int size = 10;
+            array = new int[size];
+
+            Console.WriteLine("start");
+            CreateArray(array);
+            Console.WriteLine("Insertion sorting");
+            InsertionSorter ISsort = new InsertionSorter();
+            ISsort.Sort(array);
+            ISsort.Print(array);
+            Console.WriteLine("end");
+
+            Console.WriteLine("start");
+            CreateArray(array);
+            Console.WriteLine("Buble sorting");
+            BubbleSorter BSsort = new BubbleSorter();
+            BSsort.Sort(array);
+            BSsort.Print(array);
+            Console.WriteLine("end");
+
+            Console.ReadKey();
+        }
+        static void CreateArray(int[] array)
+        {
+            Random rand = new Random();
+            for (int item = 0; item < array.Length; item++)
+            {
+                array[item] = rand.Next(100);
+            }
+            foreach (int value in array)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
