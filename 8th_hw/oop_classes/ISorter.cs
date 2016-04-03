3e95c4ab1f40b6f7edff9af48156311fe2cb7cf5
Iaroslav Paseka <saserty@ukr.net>
2016-04-03 17:22:53 +0300

    8(

---
+++ 8th_hw/oop_classes/ISorter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_classes
+{
+    public interface ISorter<T> : IPrintable<T> where T : IComparable
+    {
+        void Sort();
+        void Swap(int a, int b);
+    }
+}
---
+++ 8th_hw/oop_classes/Sorter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_classes
+{
+    public abstract class Sorter<T> : ISorter<T> where T : IComparable
+    {
+        protected T[] SortArray;
+
+        public abstract void Sort();
+
+        public void Swap(int a, int b)
+        {
+            T temp = SortArray[b];
+            SortArray[b] = SortArray[a];
+            SortArray[a] = temp;
+        }
+
+        public void Print()
+        {
+            foreach (T value in SortArray)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
